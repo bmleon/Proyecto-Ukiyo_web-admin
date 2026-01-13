67b908cd59971ec51788813e8d245a9f6ec90593
Belén María León <belenleon10.98@gmail.com>
2026-01-13 19:30:50 +0100

    modificando ecosystem.config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'ukiyo-admin',
-      port: '3000',
+      port: '3001', // CAMBIO: Usamos el 3001 para que no choque con el 3000
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs' // Esta es la ruta donde Nuxt construye la app
+      script: './.output/server/index.mjs'
     }
   ]
 }
