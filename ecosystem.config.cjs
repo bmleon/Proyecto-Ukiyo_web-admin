8b35fd9e10172b87250bfd3a65f6f176f364cd59
Belén María León <belenleon10.98@gmail.com>
2026-01-13 20:56:54 +0100

    Forzar puerto 3001 en PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'ukiyo-admin',
-      port: '3001', // CAMBIO: Usamos el 3001 para que no choque con el 3000
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs',
+      // AQUÍ ESTÁ EL CAMBIO: Forzamos las variables de entorno
+      env: {
+        PORT: 3001,
+        HOST: '0.0.0.0',
+        NODE_ENV: 'production'
+      }
     }
   ]
 }
